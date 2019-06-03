3054cac3cbd46f74e2410a3dc08a4ba1e6ca792e
Oton <49622692+Oton67@users.noreply.github.com>
2019-06-03 21:11:33 +0200

    C# register/login

---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Diagnostics;
+
+namespace Item_menagment
+{
+    public partial class Form1 : Form
+    {
+        string connection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|testlogin.mdf;Integrated Security=True;Connect Timeout=30";
+        int LoginID;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+           // Process.GetCurrentProcess().Kill();
+            Application.Exit();
+        }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(connection);
+
+
+            MD5 md5 = new MD5CryptoServiceProvider();
+            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(password.Text.Trim()));
+            byte[] result = md5.Hash;
+
+            StringBuilder strBuilder = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                strBuilder.Append(result[i].ToString("x2"));
+            }
+
+            SqlDataAdapter sda = new SqlDataAdapter("select count(*) from login where username ='"+username.Text+"' and password='"+strBuilder.ToString()+"'", conn);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+
+
+            SqlCommand oCmd = new SqlCommand("SELECT id from login where username ='" + username.Text + "'", conn);
+            conn.Open();
+            using (SqlDataReader oReader = oCmd.ExecuteReader())
+            {
+                while (oReader.Read())
+                {
+                    LoginID = Convert.ToInt32(oReader["id"]);
+                   // MessageBox.Show(matchingUsername);
+                }
+
+                conn.Close();
+            }
+     
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                if (LoginID == 1 || LoginID == 2)
+                {
+                    this.Hide();
+                    AdminMain admin = new AdminMain();
+                    admin.Show();
+                }
+
+                else
+                {
+                    this.Hide();
+                    Main user = new Main(LoginID);
+                    user.Show();
+                }
+            }
+            else MessageBox.Show("Please enter the correct username and password", "alert", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Register_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Register register = new Register();
+            register.Show();
+        }
+    }
+}
---
+++ Register.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Diagnostics;
+
+
+namespace Item_menagment
+{
+    public partial class Register : Form
+    {
+        string connection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|testlogin.mdf;Integrated Security=True;Connect Timeout=30";
+        public Register()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void Register_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Process.GetCurrentProcess().Kill();
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Reg_Click(object sender, EventArgs e)
+        {
+            int flag = 0;
+
+            using (SqlConnection sqlCon = new SqlConnection(connection))
+            {
+                sqlCon.Open();
+                string CheckUsername;
+
+                SqlCommand oCmd = new SqlCommand("SELECT username from login", sqlCon);
+                using (SqlDataReader oReader = oCmd.ExecuteReader())
+                {
+                    while (oReader.Read())
+                    {
+                        CheckUsername = (oReader["username"]).ToString();
+                        if (CheckUsername == username.Text.ToString() || CheckUsername == username.Text.ToString().ToLower() || CheckUsername == username.Text.ToString().ToUpper()) flag = 1;
+                    }
+
+                }
+                sqlCon.Close();
+            }
+            if (flag == 1) MessageBox.Show("Username already exists!");
+            else if (username.Text.ToString().ToLower() == "admin") MessageBox.Show("Can't make ADMIN account!");
+            else if (username.Text == "" || password.Text == "") MessageBox.Show("Please fill all the fields!");
+            else if (password.Text != Cpassword.Text) MessageBox.Show("Password does not match!");
+            else
+            {
+
+                MD5 md5 = new MD5CryptoServiceProvider();
+                md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(password.Text.Trim()));
+                byte[] result = md5.Hash;
+
+                StringBuilder strBuilder = new StringBuilder();
+                for (int i = 0; i < result.Length; i++)
+                {
+                    strBuilder.Append(result[i].ToString("x2"));
+                }
+
+                using (SqlConnection sqlCon = new SqlConnection(connection))
+                {
+                    sqlCon.Open();
+                    SqlCommand sqlCmd = new SqlCommand("UserAdd", sqlCon);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("@username", username.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@password", strBuilder.ToString());
+                    sqlCmd.Parameters.AddWithValue("@FirstName", FirstName.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@LastName", LastName.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Email", Email.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Contact", Contact.Text.Trim());
+                    sqlCmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Registration Successgull!");
+                    Clear();
+
+                    this.Close();
+                    Form1 login = new Form1();
+                    login.Show();
+                }
+            }
+        }
+
+        void Clear()
+        {
+            username.Text = password.Text = Cpassword.Text = FirstName.Text = LastName.Text = Email.Text = Contact.Text = "";
+        }
+
+        private void Back_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Form1 login = new Form1();
+            login.Show();
+        }
+
+        private void Number_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Char chr = e.KeyChar;
+            if (Char.IsDigit(chr))
+            {
+                e.Handled = true;
+                MessageBox.Show("Please enter a valid value!");
+            }
+            
+        }
+
+        private void Letter_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Char chr = e.KeyChar;
+            if (Char.IsLetter(chr))
+            {
+                e.Handled = true;
+                MessageBox.Show("Please enter a valid value!");
+            }
+            if (char.IsWhiteSpace(chr))
+            {
+                e.Handled = true;
+                MessageBox.Show("No spaces!");
+            }
+        }
+
+        private void IsSpace(object sender, KeyPressEventArgs e)
+        {
+            Char chr = e.KeyChar;
+            if (char.IsWhiteSpace(chr))
+            {
+                e.Handled = true;
+                MessageBox.Show("No spaces!");
+            }
+        }
+
+
+    }
+}
